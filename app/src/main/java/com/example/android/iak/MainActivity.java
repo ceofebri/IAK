d0e1023f4065eb46ced594f6fac602f53e6d186f
ceofebri <gebrinan@gmail.com>
2018-01-28 17:05:03 +0700

    Image View with Picasso

--- app/src/main/java/com/example/android/iak/MainActivity.java
+++ app/src/main/java/com/example/android/iak/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.android.iak;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -14,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv_username;
     private TextView tv_password;
+    private ImageView iv_picture;
+    private String link_picture = "http://st.motortrend.com/uploads/sites/10/2016/08/2017_Nissan_GT_R_gray_11.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
         tv_username = findViewById(R.id.tv_username);
         tv_password = findViewById(R.id.tv_password);
+        iv_picture = findViewById(R.id.iv_picture);
 
         String username_intent = getIntent().getStringExtra(USERNAME);
         String password_intent = getIntent().getStringExtra(PASSWORD);
 
         tv_username.setText(username_intent);
         tv_password.setText(password_intent);
+
+        Picasso.with(this)
+                .load(link_picture)
+                .into(iv_picture, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "Berhasil", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError() {
+                        Toast.makeText(MainActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 
     @Override
